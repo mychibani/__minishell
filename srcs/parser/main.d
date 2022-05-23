19e28d669c37f669e37f71047326aa2c20a3dc86
mychibani <m.yassinechibani@gmail.com>
2022-05-23 15:36:41 +0200

    setup errors_files, libft included, printf too, begin builtin code

--- srcs/parser/main.d
+++
@@ -1,4 +0,0 @@
-srcs/parser/main.o: srcs/parser/main.c includes/minishell.h \
- includes/define.h includes/struct.h includes/enum.h \
- libft/includes/libft.h libft/includes/define.h libft/includes/struct.h \
- libft/includes/fonctions.h includes/fonctions.h
